4a817fd3d885a1da3e0f2860972fc716d96a131f
PaulWalker <91833744+Vampire421@users.noreply.github.com>
2021-10-26 01:55:08 +0530

    Update welcome.py

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -10,10 +10,10 @@ async def welcome(bot,message):
  await bot.send_message(text=f"Welcome {message.from_user.mention} to FILM DISTRICT ,  ✍️ Just Type The Actual Name Of Movie Or Webseries. If You Don't Get, You Request Then. If Write Wrong Spelling, Nothing Will Come. It Will Spoil Your Time.
 Maintaining Proper Method
 #request  
-1. Movie / Webseries Name 
+1. Movie Webseries Name 
 2. Release Year 
-3. Language (Bengali/ Hindi/ English
-4. Quality 480p/ 720p/ 1080p",chat_id=chatid)
+3. Language Bengali Hindi English
+4. Quality 480p 720p 1080p",chat_id=chatid)
  
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
