fee550b24842403309e3eb18e10eb7459a2c5273
PaulWalker <91833744+Vampire421@users.noreply.github.com>
2021-10-26 01:53:57 +0530

    Update welcome.py
    be cool

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -7,14 +7,13 @@ from pyrogram.types import Message, User
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
  chatid= message.chat.id
- await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  ✍️ Just Type The Actual Name Of Movie Or Webseries. If You Don't Get, You Request Then. If Write Wrong Spelling, Nothing Will Come. It Will Spoil Your Time.
-
+ await bot.send_message(text=f"Welcome {message.from_user.mention} to FILM DISTRICT ,  ✍️ Just Type The Actual Name Of Movie Or Webseries. If You Don't Get, You Request Then. If Write Wrong Spelling, Nothing Will Come. It Will Spoil Your Time.
 Maintaining Proper Method
 #request  
 1. Movie / Webseries Name 
 2. Release Year 
-3. Language (Bengali/ Hindi/ English)
-4. Quality (480p/ 720p/ 1080p)",chat_id=chatid)
+3. Language (Bengali/ Hindi/ English
+4. Quality 480p/ 720p/ 1080p",chat_id=chatid)
  
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
