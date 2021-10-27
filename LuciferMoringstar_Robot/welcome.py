f18da07c5060498cf543e2ec74f045e598d20ffd
AnjanModak <88320451+AnjanModak@users.noreply.github.com>
2021-10-26 21:34:40 -0700

    Update welcome.py

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message, User
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
  chatid= message.chat.id
- await bot.send_message(text=f"Welcome {message.from_user.mention} to FILM DISTRICT, Just Type The Actual Name Of The Movie or Webseries You Want If You Didn't Get The The File Then The Spelling Is Incorrect 😐",chat_id=chatid)
+ await bot.send_message(text=f"👋 Hello {message.from_user.mention} My Name Is FILM DISTRICT BOT, I Can Provide Movies/Series In This Group. Just Type The Actual Name Of The Movie/Series. You Will Get The Movie/Series If You Write Correct Spelling. If You Don't Get The Movie/Series It Is Sure That You Have Written Incorrect Spelling Or Your Requested Movie/Series Does Not Exit In My Database. 😐",chat_id=chatid)
  
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
