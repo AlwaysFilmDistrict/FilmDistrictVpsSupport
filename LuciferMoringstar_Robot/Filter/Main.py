8c74d02084c8d5c8817a6c3bab36ea5a4e0df83b
AnjanModak <88320451+AnjanModak@users.noreply.github.com>
2021-10-30 14:09:19 -0700

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded By: {message.chat.title}\n**🙋 **Requested By:** {message.from_user.mention}\n**\n**Get Support** ✔️ **HeartBeat**\n**"
+        mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded By: {message.chat.title}\n**🙋 **Requested By:** {message.from_user.mention}\n**\n**Get Support** ✔️ **HeartBeat\n**\n**"
         files = await get_filter_results(query=search)
         if files:
             for file in files:
@@ -115,7 +115,7 @@ async def group(client, message):
     if 2 < len(message.text) < 50:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded By: {message.chat.title}\n**🙋 **Requested By:** {message.from_user.mention}\n**\n**Get Support** ✔️ **HeartBeat**\n**"
+        mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded By: {message.chat.title}\n**🙋 **Requested By:** {message.from_user.mention}\n**\n**Get Support** ✔️ **HeartBeat\n**\n**"
         nyva=BOT.get("username")
         if not nyva:
             botusername=await client.get_me()
