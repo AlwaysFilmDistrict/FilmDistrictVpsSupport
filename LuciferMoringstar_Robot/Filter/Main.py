cd00ed7d2c68d57fb02ca585f13a3e30348722e7
AnjanModak <88320451+AnjanModak@users.noreply.github.com>
2021-11-03 23:23:20 -0700

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = Data: f"** Title:<a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year:<a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n**🙋 **Requested By:** {message.from_user.mention}\n**\n**Get Support ✔️ HeartBeat\n**"
+        mo_tech_yt = f"** Title:<a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year:<a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n**🙋 **Requested By:** {message.from_user.mention}\n**\n**Get Support ✔️ HeartBeat\n**"
         files = await get_filter_results(query=search)
         if files:
             for file in files:
