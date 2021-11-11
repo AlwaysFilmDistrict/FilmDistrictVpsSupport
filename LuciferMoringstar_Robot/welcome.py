9e72ccca56d583d4f4f3809e16253ef8771d45a3
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-11-11 22:19:39 +0530

    Update welcome.py

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -3,23 +3,11 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, User
 
 
-
-#@Client.on_message(filters.new_chat_members)
-#async def welcome(bot,message):
-# chatid= message.chat.id
-# await bot.send_message(text=
-
 WELCOME_TEXT = """👋 Hello {} Welcome To {}
 My Name Is FILM DISTRICT BOT, I Can Provide Movies/Series In This Group.
 Just Type The Actual Name Of The Movie/Series.
 You Will Get The Movie/Series If You Write Correct Spelling.
 If You Don't Get The Movie/Series It Is Sure That You Have Written Incorrect Spelling Or Your Requested Movie/Series Does Not Exit In My Database. 😐"""
-
-
-#@Client.on_message(filters.left_chat_member)
-#async def goodbye(bot,message):
-# chatid= message.chat.id
-# await bot.send_message(text=f
 
 GOOD_BYE_TEXT = """Bye {} , Have a Nice Day"""
 
