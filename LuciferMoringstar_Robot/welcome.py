84a1b5c71dfba3789bd4569819fdc4beb9a76392
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-11-11 22:27:40 +0530

    Update welcome.py

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -15,12 +15,10 @@ GOOD_BYE_TEXT = """Bye {} , Have a Nice Day"""
 
 @Client.on_message(filters.new_chat_members)
 async def auto_welcome(bot, msg):
-        Auto_Delete=await msg.reply_text(text=WELCOME_TEXT.format(
+        await msg.reply_text(text=WELCOME_TEXT.format(
             mention = msg.from_user.mention,
             groupname = msg.chat.title
         )
-        await asyncio.sleep(60) # in seconds
-        await Auto_Delete.delete()
-
+        
 
 
