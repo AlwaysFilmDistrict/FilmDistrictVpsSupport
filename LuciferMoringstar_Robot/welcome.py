baed5d0ea2883397646506584d6d584e095e9158
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-11-11 22:32:33 +0530

    Update welcome.py

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -2,20 +2,21 @@ import asyncio, os
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-
-WELCOME_TEXT = """👋 Hello {} Welcome To {}
-My Name Is FILM DISTRICT BOT, I Can Provide Movies/Series In This Group.
-Just Type The Actual Name Of The Movie/Series.
-You Will Get The Movie/Series If You Write Correct Spelling.
-If You Don't Get The Movie/Series It Is Sure That You Have Written Incorrect Spelling Or Your Requested Movie/Series Does Not Exit In My Database. 😐"""
-
 GOOD_BYE_TEXT = """Bye {} , Have a Nice Day"""
-
-
 
 @Client.on_message(filters.new_chat_members)
 async def auto_welcome(bot, message):
-    await message.reply_text(text="hi")
+    username = message.from_user.mention
+    chatname = message.chat.name
+    Auto_Delete=await message.reply_text(
+        text = f"""👋 Hello {username} Welcome To {chatname}
+My Name Is FILM DISTRICT BOT, I Can Provide Movies/Series In This Group.
+Just Type The Actual Name Of The Movie/Series.
+You Will Get The Movie/Series If You Write Correct Spelling.
+If You Don't Get The Movie/Series It Is Sure That You Have Written Incorrect Spelling Or Your Requested Movie/Series Does Not Exit In My Database. 😐""")
+    await asyncio.sleep(60) # in seconds
+    await Auto_Delete.delete()
+
         
 
 
