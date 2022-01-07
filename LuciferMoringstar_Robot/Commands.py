cbc675ca28c7fca3f8d14dc497c77245828eaebd
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-01-07 13:06:20 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -111,6 +111,7 @@ OWNER_ID = set(int(x) for x in os.environ.get("ADMINS", "").split())
 
 @Client.on_message(filters.command("start"))
 async def start(bot, message):
+  
     chat_id = message.from_user.id
     if not await db.is_user_exist(chat_id):
         await db.add_user(chat_id)
@@ -120,31 +121,30 @@ async def start(bot, message):
         )
     usr_cmdall1 = message.text
     if usr_cmdall1.startswith("/start pr0fess0r_99"):
-
         if AUTH_CHANNEL:
             invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
             try:
-                user = await bot.get_chat_member(int(AUTH_CHANNEL), cmd.from_user.id)
+                user = await bot.get_chat_member(int(AUTH_CHANNEL), message.from_user.id)
                 if user.status == "kicked":
                     await bot.send_message(
-                        chat_id=cmd.from_user.id,
+                        chat_id=message.from_user.id,
                         text="Sorry Sir, You are Banned to use me.",
                         parse_mode="markdown",
                         disable_web_page_preview=True
                     )
                     return
             except UserNotParticipant:
-                ident, file_id = cmd.text.split("_-_-_-_")
+                ident, file_id = message.text.split("_-_-_-_")
                 await bot.send_message(
-                    chat_id=cmd.from_user.id,
+                    chat_id=message.from_user.id,
                     text="**Please Join My Updates Channel to use this Bot!**",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                                InlineKeyboardButton("📢 Join Updates Channel 📢", url=invite_link.invite_link)
                             ],
                             [
-                                InlineKeyboardButton(" 🔄 Try Again", callback_data=f"checksub#{file_id}")
+                                InlineKeyboardButton("🔄 Try Again", callback_data=f"checksub#{file_id}")
                             ]
                         ]
                     ),
@@ -153,18 +153,18 @@ async def start(bot, message):
                 return
             except Exception:
                 await bot.send_message(
-                    chat_id=cmd.from_user.id,
+                    chat_id=message.from_user.id,
                     text="Something went Wrong.",
                     parse_mode="markdown",
                     disable_web_page_preview=True
                 )
                 return
         try:
-            ident, file_id = cmd.text.split("_-_-_-_")
+            ident, file_id = message.text.split("_-_-_-_")
             filedetails = await get_file_details(file_id)
             for files in filedetails:
                 title = files.file_name
-                size=get_size(files.file_size)
+                size=files.file_size
                 f_caption=files.caption
                 if CUSTOM_FILE_CAPTION:
                     try:
@@ -174,28 +174,27 @@ async def start(bot, message):
                         f_caption=f_caption
                 if f_caption is None:
                     f_caption = f"{files.file_name}"
-                buttons = [
-                    [
-                        InlineKeyboardButton('Search again', switch_inline_query_current_chat='')
-                    ]
-                    ]
+                buttons = [[
+                  InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat='')
+                   ]]
+                    
                 await bot.send_cached_media(
-                    chat_id=cmd.from_user.id,
+                    chat_id=message.from_user.id,
                     file_id=file_id,
                     caption=f_caption,
                     reply_markup=InlineKeyboardMarkup(buttons)
                     )
         except Exception as err:
-            await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
-    elif len(cmd.command) > 1 and cmd.command[1] == 'subscribe':
+            await message.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
+    elif len(message.command) > 1 and message.command[1] == 'subscribe':
         invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
         await bot.send_message(
-            chat_id=cmd.from_user.id,
+            chat_id=message.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                        InlineKeyboardButton("📢 Join Updates Channel 📢", url=invite_link.invite_link)
                     ]
                 ]
             )
