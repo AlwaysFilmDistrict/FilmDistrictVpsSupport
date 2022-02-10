396806c2bcd8634c3f685cb7ba88f1fdd63bf423
HeartBeat <88320451+AnjanModak@users.noreply.github.com>
2022-02-10 01:20:22 -0800

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,12 +89,10 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('🔗 FD 2.0', url='https://telegram.me/joinchat/BOMKAM_4u0ozNWU1'),
+        InlineKeyboardButton('♨️ FD UPDATES', url='https://telegram.me/joinchat/EUUS8b0iEnVjZTU9')
         ],[
-        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
-        ],[
-        InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
+        InlineKeyboardButton('🔍 Search Again 🔎', switch_inline_query_current_chat=query)
         ]]
     return InlineKeyboardMarkup(buttons)
 
