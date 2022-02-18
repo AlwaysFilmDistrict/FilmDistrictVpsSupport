76fa999a3e664ab91eb1bc42e46730e6ab5b2472
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-02-18 15:10:59 +0530

    Update imdb.py

--- LuciferMoringstar_Robot/modules/imdb.py
+++ LuciferMoringstar_Robot/modules/imdb.py
@@ -1,12 +1,8 @@
 from pyrogram import Client, filters
 import os
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
-from LuciferMoringstar_Robot.func.imdb_information import get_poster
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-
-
-IMDBOT_CAPTION = "hi"
 
 @Client.on_message(filters.command(["imdb", 'search']))
 async def imdb_search(client, message):
@@ -28,85 +24,3 @@ async def imdb_search(client, message):
     else:
         await message.reply('Give me a movie Name')
 
-
-@Client.on_callback_query(filters.regex('^imdb'))
-async def imdb_callback(bot: Client, query: CallbackQuery):
-    i, movie = query.data.split('#')
-    imdb = await get_poster(query=movie, id=True)
-    btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"{imdb.get('title')} - {imdb.get('year')}",
-                    url=imdb['url'],
-                )
-            ]
-        ]
-    if imdb.get('poster'):
-        await query.message.reply_photo(
-            photo=imdb['poster'],
-            caption=IMDBOT_CAPTION.format(
-                query = imdb['title'],
-                title = imdb['title'],
-                votes = imdb['votes'],
-                aka = imdb["aka"],
-                seasons = imdb["seasons"],
-                box_office = imdb['box_office'],
-                localized_title = imdb['localized_title'],
-                kind = imdb['kind'],
-                imdb_id = imdb["imdb_id"],
-                cast = imdb["cast"],
-                runtime = imdb["runtime"],
-                countries = imdb["countries"],
-                certificates = imdb["certificates"],
-                languages = imdb["languages"],
-                director = imdb["director"],
-                writer = imdb["writer"],
-                producer = imdb["producer"],
-                composer = imdb["composer"],
-                cinematographer = imdb["cinematographer"],
-                music_team = imdb["music_team"],
-                distributors = imdb["distributors"],
-                release_date = imdb['release_date'],
-                year = imdb['year'],
-                genres = imdb['genres'],
-                poster = imdb['poster'],
-                plot = imdb['plot'],
-                rating = imdb['rating'],
-                url = imdb['url']
-            ),
-            reply_markup=InlineKeyboardMarkup(btn))
-        await query.message.delete()
-    else:
-        await query.message.edit(
-            text=IMDBOT_CAPTION.format(
-                query = imdb['title'],
-                title = imdb['title'],
-                votes = imdb['votes'],
-                aka = imdb["aka"],
-                seasons = imdb["seasons"],
-                box_office = imdb['box_office'],
-                localized_title = imdb['localized_title'],
-                kind = imdb['kind'],
-                imdb_id = imdb["imdb_id"],
-                cast = imdb["cast"],
-                runtime = imdb["runtime"],
-                countries = imdb["countries"],
-                certificates = imdb["certificates"],
-                languages = imdb["languages"],
-                director = imdb["director"],
-                writer = imdb["writer"],
-                producer = imdb["producer"],
-                composer = imdb["composer"],
-                cinematographer = imdb["cinematographer"],
-                music_team = imdb["music_team"],
-                distributors = imdb["distributors"],
-                release_date = imdb['release_date'],
-                year = imdb['year'],
-                genres = imdb['genres'],
-                poster = imdb['poster'],
-                plot = imdb['plot'],
-                rating = imdb['rating'],
-                url = imdb['url']
-            ),
-            reply_markup=InlineKeyboardMarkup(btn))
-    await query.answer()
