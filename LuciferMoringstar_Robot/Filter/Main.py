3e7c9515f80994a43eaf611be00c351a9dc13802
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-18 11:28:24 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -398,8 +398,7 @@ async def group_filters(client, message):
                 text += "📌 **This Post Will Be Deleted After 10 Minutes**"      
                 IMDB_CAPTION = os.environ.get('IMDB_POSTER_CAPTION', text)
                 cap = IMDB_CAPTION.format(
-                    first_name = message.from_user.first_name,
-                    user_id = f"tg://user?id={message.from_user.id}",
+                    mention = [{message.from_user.first_name}](tg://user?id={message.from_user.id})",
                     chat_name = message.chat.title,
                     query = search,
                     title = imdb['title'],
@@ -502,8 +501,7 @@ async def group_filters(client, message):
             Text += "📌 **This Post Will Be Deleted After 10 Minutes**"      
             IMDB_CAPTION = os.environ.get('IMDB_POSTER_CAPTION', Text)
             cap = IMDB_CAPTION.format(
-                first_name = message.from_user.first_name,
-                user_id = f"tg://user?id={message.from_user.id}",
+                mention = [{message.from_user.first_name}](tg://user?id={message.from_user.id})",
                 chat_name = message.chat.title,
                 total_page = totalss,
                 query = search,
