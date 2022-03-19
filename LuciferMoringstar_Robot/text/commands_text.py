e1078f900b270700b70e9ee3d463e462c078bcb5
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-19 12:28:13 +0530

    Create commands_text.py

---
+++ LuciferMoringstar_Robot/text/commands_text.py
@@ -0,0 +1,6 @@
+ABOUT_TEXT = f"""
+🤖 Name : [Film District Bot 2.0](t.me/{username})
+👑 Creator : [HeartBeat](t.me/helloheartbeat)
+📃 Language : Python3
+🛠 Library : Pyrogram Asyncio 1.13.0
+📦 Source Code : 🤐"""
