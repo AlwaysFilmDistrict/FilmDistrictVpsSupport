fad8bd860b2fa9d6aebcb16493f9aaa9dbbafdf5
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-19 12:32:46 +0530

    Update commands_text.py

--- LuciferMoringstar_Robot/text/commands_text.py
+++ LuciferMoringstar_Robot/text/commands_text.py
@@ -4,3 +4,24 @@ ABOUT_TEXT = """
 📃 Language : Python3
 🛠 Library : Pyrogram Asyncio 1.13.0
 📦 Source Code : 🤐"""
+
+
+HELP_TEXT_USER = """
+<u>Basic Commands</u>
+ /start : Check If Am Alive Or Dead
+ /about : About Me"""
+
+
+HELP_TEXT_DEV = """
+<u>Basic Commands</u>
+  /start : Check If Am Alive Or Dead
+  /about : About Me
+
+<u>Bot Owner Only</u>
+  /broadcast Reply Any Message Or Media
+  /stats User Status
+  /total How Many Files Added In Database
+  /logger  Get Logs
+  /delete Delete File From Database
+  /dyno Check Bot Dyno
+"""
