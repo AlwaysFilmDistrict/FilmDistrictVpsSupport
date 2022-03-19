deb629134875c52126825029b5e8be58a7dcde85
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-19 11:34:31 +0530

    Update pm_group.py

--- LuciferMoringstar_Robot/pm_group.py
+++ LuciferMoringstar_Robot/pm_group.py
@@ -1,5 +1,5 @@
 from pyrogram import Client as LuciferMoringstar_Robot, filters as Worker
-from LuciferMoringstar_Robot.Filter.Main import group_filters, pm_autofilter
+from LuciferMoringstar_Robot.models.autofilter_group import group_filters
 from Config import AUTH_GROUPS, AUTH_USERS, ADMINS
 
 @LuciferMoringstar_Robot.on_message(Worker.text & Worker.group & Worker.incoming & Worker.chat(AUTH_GROUPS) if AUTH_GROUPS else Worker.text & Worker.group & Worker.incoming)
@@ -9,7 +9,6 @@ async def groupfilters(client, message):
 
 @LuciferMoringstar_Robot.on_message(Worker.text & Worker.private & Worker.incoming)
 async def pm_filters(client, message):
-  #  if message.from_user.id not in ADMINS:
-   #     await client.send_sticker(chat_id=message.from_user.id, sticker='CAACAgUAAxkBAAEBoPBh0wHhhDxOtO6oGj4Gy5jpKWF-NwACFAQAAh0k-FXoemcDdMDyJx4E')
-   #     return
-    await pm_autofilter(client, message)
+    if message.from_user.id not in ADMINS:
+        await client.send_sticker(chat_id=message.from_user.id, sticker='CAACAgUAAxkBAAEBoPBh0wHhhDxOtO6oGj4Gy5jpKWF-NwACFAQAAh0k-FXoemcDdMDyJx4E')
+        return
