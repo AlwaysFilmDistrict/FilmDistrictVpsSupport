5a2f0465a85467b7e064a86f6254eee106276cfd
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-19 10:59:11 +0530

    Update channels.py

--- LuciferMoringstar_Robot/admins/channels.py
+++ LuciferMoringstar_Robot/admins/channels.py
@@ -1,6 +1,7 @@
+import os
 from pyrogram import Client, filters
 from Database.autofilter_db import save_file
-from Config import CHANNELS
+from Config import CHANNELS, ADMINS
 
 media_filter = filters.document | filters.video | filters.audio
 
@@ -18,3 +19,34 @@ async def media(bot, message):
     media.file_type = file_type
     media.caption = message.caption
     await save_file(media)
+
+
+
+@Client.on_message(filters.command('channel') & filters.user(ADMINS))
+async def channel_info(bot, message):
+    """Send basic information of channel"""
+    if isinstance(CHANNELS, (int, str)):
+        channels = [CHANNELS]
+    elif isinstance(CHANNELS, list):
+        channels = CHANNELS
+    else:
+        raise ValueError("Unexpected type of CHANNELS")
+
+    text = '📑 **Indexed channels/groups**\n'
+    for channel in channels:
+        chat = await bot.get_chat(channel)
+        if chat.username:
+            text += '\n@' + chat.username
+        else:
+            text += '\n' + chat.title or chat.first_name
+
+    text += f'\n\n**Total:** {len(CHANNELS)}'
+
+    if len(text) < 4096:
+        await message.reply(text)
+    else:
+        file = 'Indexed channels.txt'
+        with open(file, 'w') as f:
+            f.write(text)
+        await message.reply_document(file)
+        os.remove(file)
