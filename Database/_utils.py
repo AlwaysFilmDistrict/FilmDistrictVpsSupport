8022ffc60346617d9d2b19ffab78b191aa9d1beb
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-19 10:22:06 +0530

    Create _utils.py

---
+++ Database/_utils.py
@@ -0,0 +1,18 @@
+from pyrogram.errors import UserNotParticipant
+from Config import AUTH_CHANNEL
+
+
+# Force Sub
+
+async def is_subscribed(bot, query):
+    try:
+        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+    except UserNotParticipant:
+        pass
+    except Exception as e:
+        logger.exception(e)
+    else:
+        if not user.status == 'kicked':
+            return True
+
+    return False
