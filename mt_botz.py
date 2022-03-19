4981ad913cf43ab80837e924ce381ba229f72b97
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-19 11:48:03 +0530

    Update mt_botz.py

--- mt_botz.py
+++ mt_botz.py
@@ -2,14 +2,21 @@ import logging
 import logging.config
 
 # Get logging configurations
+
 logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.ERROR)
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from Database.autofilter_db import Media
-from Config import API_ID, API_HASH, BOT_TOKEN
 import pyromod.listen
+from Database.autofilter_db import Media
+from Database.users_chats_db import db
+from Database._utils import temp
+from Config import API_ID, API_HASH, BOT_TOKEN
+
 
 class Bot(Client):
 
@@ -25,9 +32,15 @@ class Bot(Client):
         )
 
     async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
         await super().start()
         await Media.ensure_indexes()
         me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
         self.username = '@' + me.username
         print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
 
