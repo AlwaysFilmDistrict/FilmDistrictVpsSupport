448863387d95585835b571cca9b5d07c7dcd2db0
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-19 10:24:34 +0530

    Update _utils.py

--- Database/_utils.py
+++ Database/_utils.py
@@ -1,3 +1,4 @@
+import os
 from pyrogram.errors import UserNotParticipant
 from Config import AUTH_CHANNEL
 
@@ -30,3 +31,34 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + '𝖡'
+
+
+# About bot
+
+class temp(object):
+    BANNED_USERS = []
+    BANNED_CHATS = []
+    ME = None
+    CURRENT=int(os.environ.get("SKIP", 2))
+    CANCEL = False
+    U_NAME = None
+    B_NAME = None
+
+# Get Size
+
+def get_size(size):
+    """Get size in readable format"""
+
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
+
+# Get Button Count
+
+def split_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
