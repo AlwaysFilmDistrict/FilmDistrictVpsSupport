7d644190accad882c46a601c9ba68914d700a469
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-19 10:22:52 +0530

    Update _utils.py

--- Database/_utils.py
+++ Database/_utils.py
@@ -16,3 +16,17 @@ async def is_subscribed(bot, query):
             return True
 
     return False
+
+
+# Dyno
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: '𝖪', 2: '𝖬', 3: '𝖦', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + '𝖡'
