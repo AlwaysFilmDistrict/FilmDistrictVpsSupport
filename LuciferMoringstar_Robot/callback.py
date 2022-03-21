210697d1bcfbfd5b20b7e2ea2ba27a7aa47e5412
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-21 13:50:53 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -43,7 +43,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 for file in files:
                     file_id = file.file_id
                     btn.append(
-                        [InlineKeyboardButton(text=f"💭 {get_size(file.file_size)}", callback_data=f'luciferrobot#{file_id}'), InlineKeyboardButton(text=f"📁{file.file_name}", callback_data=f'luciferrobot#{file_id}')]
+                        InlineKeyboardButton(text=f"➠ {get_size(file.file_size)} ➠ {file.file_name}", callback_data=f'pr0fess0r_99#{file_id}')]
                     )
             if 0 < offset <= 10:
                 off_set = 0
