461f0037cf4a7d5374fbe9e7b9010eb0114e117d
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-24 18:53:49 +0530

    Update broadcast.py

--- LuciferMoringstar_Robot/admins/broadcast.py
+++ LuciferMoringstar_Robot/admins/broadcast.py
@@ -1,40 +1,84 @@
-from pyrogram import Client, filters
-import datetime, time, asyncio
-from Database.users_chats_db import db
+from Database.users_chats_db import db as mt
 from Config import ADMINS
-from Database._utils import broadcast_messages, get_size
-from Database.autofilter_db import Media
-        
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
-async def verupikkals(bot, message):
-    users = await db.get_all_users()
-    b_msg = message.reply_to_message
-    sts = await message.reply_text(text='Broadcasting your messages...')
+from Database._utils import get_size
+from Database.autofilter_db import Media        
+import os, asyncio, aiofiles, aiofiles.os, datetime, traceback, random, string, time
+from pyrogram import filters, Client
+from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
+from Database.broadcast import db, send_msg
+
+@Client.on_message(filters.private & filters.command("broadcast") & filters.reply & filters.user(ADMINS))
+async def broadcast_(client, message):
+    
+    print("Broadcasting......")
+
+    all_users = await db.get_all_users()
+    broadcast_msg = message.reply_to_message
+    
+    while True:
+        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+        if not config.broadcast_ids.get(broadcast_id):
+            break
+
+    out = await message.reply_text(text="**Broadcast Initiated..📣**\nYou will Be Notified with log File When All The Users Are Notified 🔔")
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
-    blocked = 0
-    deleted = 0
-    failed =0
+    failed = 0
     success = 0
-    async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
-        if pti:
-            success += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
+
+    config.broadcast_ids[broadcast_id] = dict(
+        total = total_users,
+        current = done,
+        failed = failed,
+        success = success
+    )
+
+    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
+        async for user in all_users:
+
+            sts, msg = await send_msg(
+                user_id = int(user['id']),
+                message = broadcast_msg
+            )
+
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+
+            if sts == 200:
+                success += 1
+            else:
                 failed += 1
-        done += 1
-        await asyncio.sleep(2)
-        if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+
+            if sts == 400:
+                await db.delete_user(user['id'])
+
+            done += 1
+            if config.broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                config.broadcast_ids[broadcast_id].update(
+                    dict(
+                        current = done,
+                        failed = failed,
+                        success = success
+                    )
+                )
+    if config.broadcast_ids.get(broadcast_id):
+        config.broadcast_ids.pop(broadcast_id)
+    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+
+    await asyncio.sleep(3)    
+    await out.delete()
+
+    if failed == 0:
+        await message.reply_text(text=f"""**📣 Broadcast Completed in** - `{completed_in}`\n\nTotal Users {total_users}.\nTotal Done {done}, {success} Success & {failed} Failed.""", quote=True)        
+    else:
+        await message.reply_document(document='broadcast.txt', caption=f"""** 📣 Broadcast Completed in **- `{completed_in}`\n\nTotal Users {total_users}.\nTotal Done {done}, {success} Success & {failed} Failed.""", quote=True)
+
+    await aiofiles.os.remove('broadcast.txt')
+
+
 
 
 
@@ -49,7 +93,7 @@ async def get_ststs(bot, message):
     LuciferMoringstar_Robot = await message.reply('Fetching stats..')
     total_users = await db.total_users_count()    
     files = await Media.count_documents()
-    size = await db.get_db_size()
+    size = await mt.get_db_size()
     free = 536870912 - size
     size = get_size(size)
     free = get_size(free)
