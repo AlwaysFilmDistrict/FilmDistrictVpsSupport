bc27a5045dc632a3283d3a0400779251d25746dc
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-27 10:47:36 +0530

    Create banned.py

---
+++ LuciferMoringstar_Robot/admins/banned.py
@@ -0,0 +1,19 @@
+from Database.users_chats_db import db
+from Database._utils import get_size, temp
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+async def banned_users(_, client, message: Message):
+    return (
+        message.from_user is not None or not message.sender_chat
+    ) and message.from_user.id in temp.BANNED_USERS
+
+banned_user = filters.create(banned_users)
+
+@Client.on_message(filters.private & banned_user & filters.incoming)
+async def ban_reply(bot, message):
+    ban = await db.get_ban_status(message.from_user.id)
+    await message.reply(f'Sorry Dude, You are Banned to use Me. \nBan Reason: {ban["ban_reason"]}')
+
+
