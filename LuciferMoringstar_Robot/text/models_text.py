b78853a6277005bcc1e781d87fded7af5e553d59
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-28 16:15:47 +0530

    Create models_text.py

---
+++ LuciferMoringstar_Robot/text/models_text.py
@@ -0,0 +1,54 @@
+Broadcast_text = """
+📡 Broadcast 
+Module :- Broadcast Message
+
+- A Module For Broadcast Any Message Or Media
+
+📚 AVAIBLE COMMANDS :-
+
+/broadcast Reply Any Message Or Media"""
+
+status_text = """
+👥 Status
+Module :- All Status
+
+- A Module For Check All Kind Of Status 
+
+📚 AVAIBLE COMMANDS :-
+
+/stats User Status"""
+
+database_text = """
+🗃 Database
+Module :- File Database
+
+- A Module To Fetch Database
+
+📚 AVAIBLE COMMANDS :-
+
+/delete Delete File From Database
+/total How Many Files Added In Database"""
+
+logs_text = """
+⚠️ Logs
+Module :- Get Logs
+
+- A Module To Fetch All Logs
+
+📚 AVAIBLE COMMANDS :-
+
+/logger  Get Logs"""
+
+ban_pm_user_text = """
+❌ Ban
+Module :- Ban & Unban User
+
+- A Module To Use Ban & Unban User
+
+📚 AVAIBLE COMMANDS :-
+
+/ban - Userid / Username
+/unban - Userid / Username """
+
+
+
