9191058a2a87889a513151f2201792bda449e9e1
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-29 07:01:01 +0530

    Update _utils.py

--- Database/_utils.py
+++ Database/_utils.py
@@ -1,7 +1,10 @@
-import logging
+import logging, asyncio 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 import os, re, imdb
+from pyrogram.types import Message
+from typing import Union 
+from datetime import datetime
 from pyrogram.errors import InputUserDeactivated, UserIsBlocked, PeerIdInvalid, FloodWait, UserNotParticipant
 from Database.users_chats_db import db 
 from Config import AUTH_CHANNEL, MAX_LIST_ELM, LONG_IMDB_DESCRIPTION
@@ -21,7 +24,6 @@ async def is_subscribed(bot, query):
     return False
 
 # Broadcast
-
 async def broadcast_messages(user_id, message):
     try:
         await message.copy(chat_id=user_id)
@@ -82,14 +84,12 @@ def get_size(size):
     return "%.2f %s" % (size, units[i])
 
 # Get Button Count
-
 def split_list(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
 # Imdb Info
 imdb = imdb.IMDb()
-
 async def get_poster(query, bulk=False, id=False):
     if not id:
         # https://t.me/GetTGLink/4183
@@ -165,8 +165,6 @@ async def get_poster(query, bulk=False, id=False):
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
 
-
-
 def list_to_str(k):
     if not k:
         return "N/A"
@@ -178,3 +176,72 @@ def list_to_str(k):
     else:
         return ' '.join(f'{elem}, ' for elem in k)
 
+
+# Get File Id
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
+
+# Last Online
+def last_online(from_user):
+    time = ""
+    if from_user.is_bot:
+        time += "🤖 Bot :("
+    elif from_user.status == 'recently':
+        time += "Recently"
+    elif from_user.status == 'within_week':
+        time += "Within the last week"
+    elif from_user.status == 'within_month':
+        time += "Within the last month"
+    elif from_user.status == 'long_time_ago':
+        time += "A long time ago :("
+    elif from_user.status == 'online':
+        time += "Currently Online"
+    elif from_user.status == 'offline':
+        time += datetime.fromtimestamp(from_user.last_online_date).strftime("%a, %d %b %Y, %H:%M:%S")
+    return time
+
+# Etract User
+def extract_user(message: Message) -> Union[int, str]:
+    """extracts the user from a message"""
+    # https://github.com/SpEcHiDe/PyroGramBot/blob/f30e2cca12002121bad1982f68cd0ff9814ce027/pyrobot/helper_functions/extract_user.py#L7
+    user_id = None
+    user_first_name = None
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        user_first_name = message.reply_to_message.from_user.first_name
+
+    elif len(message.command) > 1:
+        if (
+            len(message.entities) > 1 and
+            message.entities[1].type == "text_mention"
+        ):
+           
+            required_entity = message.entities[1]
+            user_id = required_entity.user.id
+            user_first_name = required_entity.user.first_name
+        else:
+            user_id = message.command[1]
+            # don't want to make a request -_-
+            user_first_name = user_id
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            pass
+    else:
+        user_id = message.from_user.id
+        user_first_name = message.from_user.first_name
+    return (user_id, user_first_name)
