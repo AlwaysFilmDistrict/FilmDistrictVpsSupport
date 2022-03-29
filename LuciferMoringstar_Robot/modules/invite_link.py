16c3fde7c701c0d5e09fee0fc24faafb1d67898f
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-29 10:45:08 +0530

    Update invite_link.py

--- LuciferMoringstar_Robot/modules/invite_link.py
+++ LuciferMoringstar_Robot/modules/invite_link.py
@@ -3,10 +3,24 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["link"]))
 async def create_link(bot, message):
+    text = """
+♨️♨️ Are You Movie Lover ? ♨️
 
-    AUTH_CHANNEL = int(-1001647516287)
-    link=await bot.create_chat_invite_link(AUTH_CHANNEL)
-    await message.reply_text(
-        text=f"""
-➡️ **Invite Link**
-   **Link** {link.invite_link}""")
+🎬 Then You Are Welcomed To My Group For A Daily Breeze Of Movies
+༺━━━━━━━ ✧ ━━━━━━━༻
+ 
+📌 Old & New Movies/Series
+ 
+📌 Proper HD, DVD-Rip & Tv-Rip
+
+📌 Available In Various Size
+
+📌 Bengali | Hindi | English & More
+
+༺━━━━━━━ ✧ ━━━━━━━༻
+
+✔️ Group - https://telegram.me/joinchat/BOMKAM_4u0ozNWU1
+
+👆Click Link For Join Group"""
+
+    await message.reply_text(text=text)
