1e78ee7c4ec1e2e2071dd8132ede43a44b7bd833
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-29 07:13:58 +0530

    Update invite_link.py

--- LuciferMoringstar_Robot/modules/invite_link.py
+++ LuciferMoringstar_Robot/modules/invite_link.py
@@ -3,9 +3,8 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["link"]))
 async def create_link(bot, message):
-    link = message.text.split(" ", 1)[1]
-    get = f"{link}"
-    AUTH_CHANNEL = int(get)
+
+    AUTH_CHANNEL = int(-1001647516287)
     link=await bot.create_chat_invite_link(AUTH_CHANNEL)
     await message.reply_text(
         text=f"""
