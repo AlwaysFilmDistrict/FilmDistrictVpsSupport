0b01d7962a3dbcc969fa195c84785c9df4d3d470
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-29 07:15:47 +0530

    Update ids.py

--- LuciferMoringstar_Robot/modules/ids.py
+++ LuciferMoringstar_Robot/modules/ids.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from pyrogram.errors import UserNotParticipant
 from io import BytesIO
 
-BUTTON = [[ InlineKeyboardButton("✅️ 𝗦𝗨𝗕𝗦𝗖𝗥𝗜𝗕𝗘 ✅️", url="https://t.me/joinchat/8mwoQFL0drRhOTY1") ]]
+BUTTON = [[ InlineKeyboardButton("✅️ 𝗦𝗨𝗕𝗦𝗖𝗥𝗜𝗕𝗘 ✅️", url="https://t.me/+NIZ-lWsOd280YmQ1") ]]
 
 # ---------------+ Group ID + ---------------- #
 
