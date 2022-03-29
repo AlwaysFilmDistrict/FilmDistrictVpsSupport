3351b557e602afab403e0d99de5046503ecc97d7
HeartBeat <88320451+AnjanModak@users.noreply.github.com>
2022-03-30 00:45:06 +0530

    Update commands_text.py

--- LuciferMoringstar_Robot/text/commands_text.py
+++ LuciferMoringstar_Robot/text/commands_text.py
@@ -1,12 +1,12 @@
 START_USER_TEXT = """
-<b><i>Hello 🙋‍ [{first_name}](tg://user?id={id}),
+<b><i>Hello 👋 [{first_name}](tg://user?id={id}),
 I Am Not Only [Film District Bot](t.me/{bot_username}) To Assist You But Also Employed At Film District Group By [HeartBeat](t.me/helloheartbeat) So You Can't Get My Service By Adding Me To Your Group So Don't Waste Your Time & Data 😉
 
 Better You Click Below & Join Film District & Feel The Experience Of Downloading Unlimited Movies/ Series ✅</i></b>"""
 
 
 START_DEV_TEXT = """
-<b><i>Hello 🙋‍ [{first_name}](tg://user?id={id}),
+<b><i>Hello 👋 [{first_name}](tg://user?id={id}),
 I Am Not Only [Film District Bot](t.me/{bot_username}) To Assist You But Also Employed At Film District Group By [HeartBeat](t.me/helloheartbeat) So You Can't Get My Service By Adding Me To Your Group So Don't Waste Your Time & Data 😉
 
 Better You Click Below & Join Film District & Feel The Experience Of Downloading Unlimited Movies/ Series ✅</i></b>"""
@@ -17,11 +17,11 @@ Better You Click Below & Join Film District & Feel The Experience Of Downloading
 ABOUT_TEXT = """
 ꧁𓊈FILM DISTRICT BOT𓊉꧂
 
-𒆜 Creator : HeartBeat
-★ Language : Python3
-★ Hosted : Heroku
-★ Version : 9.0.1
-★ Framework : Pyrogram
+ッ Creator : HeartBeat
+⊱ Language : Python3
+⊱ Hosted : Heroku
+⊱ Version : 9.0.1
+⊱ Framework : Pyrogram
 ★ Database : MongoDB
 𒆜 Bot : 🇮🇳"""
 
@@ -33,7 +33,7 @@ HELP_TEXT_USER = """
 
 
 HELP_TEXT_DEV = """
-<b><i>Hello 🙋‍ {},
+<b><i>Hello 👋 {},
 
 I Can Guide You Through All Of [Film District Bot](http://t.me/FilmDistrict_Bot)'s Cool Features & How To Properly Use Them. Use The Buttons Below To Navigate Through All Of The Modules</i></b>
 """
