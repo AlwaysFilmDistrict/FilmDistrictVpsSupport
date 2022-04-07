71cf561cfdb7f98cb4544d7164e88b2b45466cdc
HeartBeat <88320451+AnjanModak@users.noreply.github.com>
2022-04-08 02:01:58 +0530

    Update song.py

--- LuciferMoringstar_Robot/modules/song.py
+++ LuciferMoringstar_Robot/modules/song.py
@@ -59,7 +59,7 @@ def a(client, message):
         )
         print(str(e))
         return
-    m.edit("`Uploading..⬆️ Please Wait...⌛`")
+    m.edit("`Uploading..⬆️ Please Wait...⏳`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
