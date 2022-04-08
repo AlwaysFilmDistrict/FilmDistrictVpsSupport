e232abe68bfc7a8d80a211bf3c882fa38d1e786a
HeartBeat <88320451+AnjanModak@users.noreply.github.com>
2022-04-09 00:31:08 +0530

    Update song.py

--- LuciferMoringstar_Robot/modules/song.py
+++ LuciferMoringstar_Robot/modules/song.py
@@ -28,7 +28,7 @@ ydl_opts = {
 @Client.on_message(filters.command(['song']))
 def song(_, message):
     query = " ".join(message.command[1:])
-    m = message.reply("🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐒𝐨𝐧𝐠 𝐎𝐧 𝐘𝐨𝐮𝐭𝐮𝐛𝐞..!\n **𝐔𝐩𝐥𝐨𝐚𝐝 𝐆𝐞𝐭𝐭𝐢𝐧𝐠 𝐒𝐥𝐨𝐰𝐞𝐝 𝐃𝐮𝐞 𝐓𝐨 𝐇𝐞𝐚𝐯𝐲 𝐓𝐫𝐚𝐟𝐟𝐢𝐜** [Learn More](https://en.m.wikipedia.org/wiki/Network_traffic)", disable_web_page_preview=True)
+    m = message.reply("🔎 Searching Song On Youtube..!\n **Upload Getting Slowed Due To Heavy Traffic** [Learn More](https://en.m.wikipedia.org/wiki/Network_traffic)", disable_web_page_preview=True)
     ydl_ops = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -41,21 +41,21 @@ def song(_, message):
         duration = results[0]["duration"]
 
     except Exception as e:
-        m.edit("❌ Sᴏʀʀʏ I ᴄᴀɴ'ᴛ Fɪɴᴅ ʏᴏᴜʀ Rᴇǫᴜᴇsᴛᴇᴅ Sᴏɴɢ 🙁.\n\nTʀʏ Aɴᴏᴛʜᴇʀ Sᴏɴɢ Nᴀᴍᴇ ᴏʀ Cʜᴇᴄᴋ Sᴘᴇʟʟɪɴɢ..!\n\nIғ ʏᴏᴜ Fᴀᴄɪɴɢ sᴀᴍᴇ ɪssᴜᴇs ғᴏʀ sᴇᴄᴏɴᴅ Tɪᴍᴇ Rᴇᴘᴏʀᴛ ɪᴛ ᴏɴ ✔️ [HeartBeat](t.me/helloheartbeat)", disable_web_page_preview=True)
+        m.edit("❌ Sorry I can't Find Your Requested Song 🙁.\n\nTry Another Song Name Or Follow Format..!\n\nIf You Facing Same Issues For Second Time Report It On ✔️ [HeartBeat](t.me/helloheartbeat)", disable_web_page_preview=True)
         print(str(e))
         return
-    m.edit("📥 𝘋𝘰𝘸𝘯𝘭𝘰𝘢𝘥𝘪𝘯𝘨 𝘚𝘰𝘯𝘨 𝘛𝘰 𝘔𝘺 𝘋𝘢𝘵𝘢𝘣𝘢𝘴𝘦...𝘗𝘭𝘦𝘢𝘴𝘦 𝘞𝘢𝘪𝘵..!")
+    m.edit("📥 Downloading Song To My Database...Please Wait..!")
     try:
         with yt_dlp.YoutubeDL(ydl_ops) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎵 Song Uploaded From Youtube Music..!.\n\nＰｏｗｅｒｅｄ　Ｂｙ ✔️ [HeartBeat](t.me/helloheartbeat)"
+        rep = f"🎵 Song Uploaded From Youtube Music...!\n\nPowered By ✔️ [HeartBeat](t.me/helloheartbeat)"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
             secmul *= 60
-        m.edit("📤 𝘜𝘱𝘭𝘰𝘢𝘥𝘪𝘯𝘨 𝘍𝘪𝘭𝘦𝘴 𝘛𝘰 𝘛𝘦𝘭𝘦𝘨𝘳𝘢𝘮...")
+        m.edit("📤 Uploading Files To Telegram...")
         message.reply_audio(
             audio_file,
             caption=rep,
@@ -66,7 +66,7 @@ def song(_, message):
         )
         m.delete()
     except Exception as e:
-        m.edit("❌ Error Contact [ƈɾҽαƚσɾ](t.me/helloheartbeat)", disable_web_page_preview=True)
+        m.edit("❌ Error Contact [HeartBeat](t.me/helloheartbeat)", disable_web_page_preview=True)
         print(e)
 
     try:
