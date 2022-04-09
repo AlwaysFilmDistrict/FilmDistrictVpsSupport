cac5a05a621c654e06e4642fe436954b94ab05eb
HeartBeat <88320451+AnjanModak@users.noreply.github.com>
2022-04-10 01:43:38 +0530

    Update mt_botz.py

--- mt_botz.py
+++ mt_botz.py
@@ -22,7 +22,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "LuciferMoringstar_Robot",
+            "FilmDistrict_Bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
