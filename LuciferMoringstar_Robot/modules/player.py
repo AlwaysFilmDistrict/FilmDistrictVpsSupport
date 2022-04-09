4cc70564196e1ad62497c2857a8a7b1bcab18c43
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-04-09 09:47:12 +0530

    Create player.py

---
+++ LuciferMoringstar_Robot/modules/player.py
@@ -0,0 +1,59 @@
+from pyrogram import Client, filters
+
+import pytgcalls
+from user import pr0fess0r
+
+calls = pytgcalls.GroupCallFactory(pr0fess0r).get_group_call()
+
+
+@Client.on_message(filters.command('play') & filters.user(ADMINS))
+async def play(_, message):
+    try:
+        await musicbot.start()
+    except:
+        print("Video/Audio: Chat")
+   
+    reply = message.reply_to_message
+
+    if not reply:
+        text = """*Give me a Audio to Play**\n\n» Use the /play command by replying to the Audio File**"""
+        await message.reply_text(text)
+    if reply:
+        fk = await message.reply('Downloading....')
+        path = await reply.download()
+        await calls.join(message.chat.id)
+        await calls.start_audio(path, repeat=False)
+        await fk.edit('**Playing...**') 
+
+
+@Client.on_message(filters.command('vplay') & filters.user(ADMINS))
+async def vplay(_, message):
+    try:
+        await musicbot.start()
+    except:
+        print("Video/Audio: Chat")
+
+    reply = message.reply_to_message
+
+    if not reply:
+        text = """*Give me a Audio to Play**\n\n» Use the /play command by replying to the Audio File**"""
+        await message.reply_text(text)
+
+    if reply:
+        fk = await message.reply('Downloading....')
+        path = await reply.download()
+        await calls.join(message.chat.id)
+        await calls.start_video(path, repeat=False)
+        await fk.edit('playing...')
+
+
+
+@Client.on_message(filters.command('leavevc') & filters.user(ADMINS))
+async def leavevc(_, message):
+    await calls.stop()
+    await calls.leave_current_group_call()
+
+
+@Client.on_message(filters.command('pause') & filters.user(ADMINS))
+async def pause(_, message):
+    await calls.pause_stream()
