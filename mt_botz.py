41d784f5f386abde4e2deb255ca0b6934c409db4
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-04-09 09:41:44 +0530

    Update mt_botz.py

--- mt_botz.py
+++ mt_botz.py
@@ -25,7 +25,7 @@ class Bot(Client):
             bot_token=BOT_TOKEN,
             workers=50,
             plugins={"root": "LuciferMoringstar_Robot"},
-            sleep_threshold=5,
+            sleep_threshold=60,
         )
 
     async def start(self):
