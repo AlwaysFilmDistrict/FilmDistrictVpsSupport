a8533e4bd4b7295169b1d4889243b5a0ec5f7a5d
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-04-18 13:36:14 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,8 @@
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN = ""
+# ∞∞ About Bot ∞∞ #
+
+API_ID = "7976184"
+API_HASH = "fd115c34d9a04c6bffd2a00f982afcd7"
+BOT_TOKEN = "5014982831:AAG2CsdZDIB8ew5vI9LFBGh9m6wde8DcpL0"
 
 # Database
 AUTOFILTER_DB = "" # Only AutoFilter 
