49abef4c639c7b578e30ef7d7a6b0028fe5416fa
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-04-18 14:42:53 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,27 +9,55 @@ def is_enabled(value, default):
     else:
         return default
 
-
-SPELLING_MODE_TEXT = environ.get('SPELLING_MODE_TEXT', 'FilmDistrict_Bot')
-SEPLLING_MODE_ON_OR_OFF = environ.get('SEPLLING_MODE_ON_OR_OFF', 'on').lower()
-BUTTON_CALLBACK_OR_URL = environ.get('BUTTON_CALLBACK_OR_URL', 'false').lower()
-BOT_PHOTO = environ.get('BOT_PHOTO')
-HEROKU_API_KEY = environ.get("HEROKU_API_KEY")
-P_TTI_SHOW_OFF = is_enabled((environ.get('BUTTON_CALLBACK_OR_URL', "False")), False)
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-IMDBOT_CAPTION = environ.get('IMDBOT_CAPTION', 'hi')
-IMDB_POSTER_ON_OFF = is_enabled((environ.get('IMDB_POSTER_ON_OFF', "False")), False)
-
-
+from keys import API_ID, API_HASH, BOT_TOKEN, BOT_PHOTO, AUTOFILTER_DB, ANOTHER_DB, ADMINS, CHANNELS, AUTH_GROUPS, FORCES_SUB, LOG_CHANNEL, SEPLLING_MODE_ON_OR_OFF, SPELLING_MODE_TEXT, IMDB_POSTER_ON_OFF, LONG_IMDB_DESCRIPTION, CUSTOM_FILE_CAPTION,
 
 
 # Bot information
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-USERSESSION = os.environ.get("USERSESSION")
+API_ID = int(environ.get('API_ID', API_ID)
+API_HASH = environ.get('API_HASH', API_HASH)
+BOT_TOKEN = environ.get('BOT_TOKEN', BOT_TOKEN) 
+BOT_PHOTO = environ.get('BOT_PHOTO', BOT_PHOTO)
 
+# MongoDB information
+DATABASE_URI = environ.get('DATABASE_2', AUTOFILTER_DB)
+DATABASE_NAME = "FilmDistrict_Bot" # Dont Change this one 
+DB_URL = os.environ.get("DATABASE_1", ANOTHER_DB)
+
+# Groups & Channels
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', CHANNELS).split()]
+auth_channel = environ.get('FORCES_SUB', FORCES_SUB)
+AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", AUTH_GROUPS).split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', LOG_CHANNEL))
+
+# Admins
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', ADMINS).split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+
+# SpellCheck
+SPELLING_MODE_TEXT = environ.get('SPELLING_MODE_TEXT', SPELLING_MODE_TEXT)
+SEPLLING_MODE_ON_OR_OFF = environ.get('SEPLLING_MODE_ON_OR_OFF', SEPLLING_MODE_ON_OR_OFF).lower()
+
+# Imdb
+IMDB_POSTER_ON_OFF = is_enabled((environ.get('IMDB_POSTER_ON_OFF', IMDB_POSTER_ON_OFF)), False)
+LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", LONG_IMDB_DESCRIPTION), False)
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+
+# Custom Caption
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+
+
+
+# 🤐
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+
+
+
+BUTTON_CALLBACK_OR_URL = environ.get('BUTTON_CALLBACK_OR_URL', 'false').lower()
+HEROKU_API_KEY = environ.get("HEROKU_API_KEY")
+P_TTI_SHOW_OFF = is_enabled((environ.get('BUTTON_CALLBACK_OR_URL', "False")), False)
+INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
+IMDBOT_CAPTION = environ.get('IMDBOT_CAPTION', 'hi')
 
 
 # Bot settings
@@ -40,15 +68,6 @@ COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 
 
-# Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('FORCES_SUB')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
-
 # User Only
 SUDO_OWNER = int(environ.get("ADMIN", "919653750"))
 SUDO_USERS = set(int(x) for x in environ.get("SUDO_USERS", "").split())
@@ -56,14 +75,6 @@ SUDO_USERS = list(SUDO_USERS)
 SUDO_USERS.append(SUDO_OWNER)
 SUDO_USERS = list(set(SUDO_USERS))
 
-
-
-# MongoDB information
-DATABASE_URI = environ['DATABASE_2']
-DATABASE_NAME = "FilmDistrict_Bot" # Dont Change this one 
-DB_URL = os.environ.get("DATABASE_1")
-
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 
@@ -72,8 +83,6 @@ START_MSG = environ.get('START_MSG')
 
 # Other
 
-LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 BOT_START_TIME = time.time() # Time
 
 
