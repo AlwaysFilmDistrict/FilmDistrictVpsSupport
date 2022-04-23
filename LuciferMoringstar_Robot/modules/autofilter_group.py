f0be1a273ddf99097320d8f663e9945dd4a00f38
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-04-23 14:09:38 +0530

    Update autofilter_group.py

--- LuciferMoringstar_Robot/modules/autofilter_group.py
+++ LuciferMoringstar_Robot/modules/autofilter_group.py
@@ -285,32 +285,19 @@ async def autofilter_download(client, query):
             )
 
 
- 
-        if imdb and imdb.get('poster'):
-            try:
+        try:
+            if imdb and imdb.get('poster'):                
                 LuciferMoringstar_Delete=await query.message.edit(text=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
                 await asyncio.sleep(600) # in seconds
                 await LuciferMoringstar_Delete.delete()
                 await client.delete_messages(query.message.chat.id,query.message.message_id)
-            except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-                pic = imdb.get('poster')
-                poster = pic.replace('.jpg', "._V1_UX360.jpg")
-                LuciferMoringstar_Delete=await query.message.edit(text=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
-                await asyncio.sleep(600) # in seconds
-                await LuciferMoringstar_Delete.delete()
-                await client.delete_messages(query.message.chat.id,query.message.message_id)
-            except Exception as e:
-                logger.exception(e)
+            else:
                 LuciferMoringstar_Delete=await query.message.edit(text=cap, reply_markup=InlineKeyboardMarkup(btn))
                 await asyncio.sleep(600) # in seconds
                 await LuciferMoringstar_Delete.delete()
-                await query.message.delete()
-        else:
-            LuciferMoringstar_Delete=await query.message.edit(text=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(600) # in seconds
-            await LuciferMoringstar_Delete.delete()
-            await query.message.delete()
-
+            await query.message.delete()      
+        except MessageNotModified:
+            pass
 
 
 
