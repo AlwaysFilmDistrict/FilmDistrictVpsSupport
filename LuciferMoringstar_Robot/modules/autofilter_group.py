ca3339b6cce0241616cef397f47d6101c8f559bd
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-04-23 19:55:04 +0530

    Update autofilter_group.py

--- LuciferMoringstar_Robot/modules/autofilter_group.py
+++ LuciferMoringstar_Robot/modules/autofilter_group.py
@@ -139,8 +139,9 @@ async def group_filters(client, message):
 <b><i>Hello 👋 {message.from_user.mention} {Get},</i></b>
 
 <b>🙏 Thanks For Request & This Is The Results You Looking For 🔍</b>"""
-        await message.reply_photo(photo=BOT_PHOTO, caption=text, reply_markup=InlineKeyboardMarkup(buttons))
-
+        Del=await message.reply_photo(photo=BOT_PHOTO, caption=text, reply_markup=InlineKeyboardMarkup(buttons))
+        await asyncio.sleep(1000)
+        await Del.delete()
 
 
 
