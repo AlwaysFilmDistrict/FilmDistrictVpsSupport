9f73ae790d44609f2008afe92c1b69069663df00
HeartBeat <88320451+AnjanModak@users.noreply.github.com>
2022-04-23 15:28:16 +0530

    Update autofilter_group.py

--- LuciferMoringstar_Robot/modules/autofilter_group.py
+++ LuciferMoringstar_Robot/modules/autofilter_group.py
@@ -137,10 +137,8 @@ async def group_filters(client, message):
 
         text = f"""
 <b>Hello 👋 {message.from_user.mention} {Get},
-🙏 Thanks For Request & This Is The Results You Looking For 🔍
 
-
-Click The Download Button To Download It</b>"""
+🙏 Thanks For Request & This Is The Results You Looking For 🔍</b>"""
         await message.reply_photo(photo=BOT_PHOTO, caption=text, reply_markup=InlineKeyboardMarkup(buttons))
 
 
