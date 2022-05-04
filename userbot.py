8771af260fc0850cf692125110ea75817248aa20
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-05-04 17:19:13 +0530

    Create userbot.py

---
+++ userbot.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+from Config API_ID, API_HASH, USER_SESSION
+UserBot = Client(USER_SESSION, API_ID, API_HASH)
+
+@UserBot.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmpermit(client: UserBot, message):    
+    mention = message.from_user.mention
+    await message.reply("Hey {mention}")
