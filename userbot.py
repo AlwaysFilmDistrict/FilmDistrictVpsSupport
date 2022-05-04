ba3c12fb4da0bbe8b06f892f441a4fd9d2f75ef4
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-05-04 18:53:45 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,10 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client
 from Config import API_ID, API_HASH, USER_SESSION
 UserBot = Client(USER_SESSION, API_ID, API_HASH)
-
-@UserBot.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
-async def pmpermit(client: UserBot, message):    
-    mention = message.from_user.mention
-    await client.send_message(chat_id=message.chat.id, text="Hey {mention}")
-
 
