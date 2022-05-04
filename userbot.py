2756b8ea512bd09a24c76acbf850a7c0e14c6055
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-05-04 18:22:27 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -7,4 +7,4 @@ async def pmpermit(client: UserBot, message):
     mention = message.from_user.mention
     await client.send_message(chat_id=message.chat.id, text="Hey {mention}")
 
-UserBot.run()
+
