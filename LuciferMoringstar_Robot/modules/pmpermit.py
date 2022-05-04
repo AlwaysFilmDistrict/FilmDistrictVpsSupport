e7fdd9ea6362d06f9860383c79a7818869eb5846
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-05-04 21:19:47 +0530

    Delete pmpermit.py

--- LuciferMoringstar_Robot/modules/pmpermit.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import filters
-from userbot import UserBot
-
-@UserBot.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
-async def pmpermit(client: UserBot, message):    
-    mention = message.from_user.mention
-    await client.send_message(chat_id=message.chat.id, text="Hey {mention}")
