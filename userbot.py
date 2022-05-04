2585274bf58c17cc9de101e28729a325c335fc78
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-05-04 17:52:52 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -5,6 +5,6 @@ UserBot = Client(USER_SESSION, API_ID, API_HASH)
 @UserBot.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmpermit(client: UserBot, message):    
     mention = message.from_user.mention
-    await message.reply("Hey {mention}")
+    await client.send_message(chat_id=message.chat.id, text="Hey {mention}")
 
 UserBot.run()
