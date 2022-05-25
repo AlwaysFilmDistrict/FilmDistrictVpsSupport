a4e1e50197d47560858151d16d136cbd431ec48a
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-05-25 05:57:14 +0530

    Update users_chats_db.py

--- Database/users_chats_db.py
+++ Database/users_chats_db.py
@@ -13,17 +13,6 @@ class Database:
         self.grp = self.db.groups
 
 
-    def new_user(self, id, name):
-        return dict(
-            id = id,
-            name = name,
-            ban_status=dict(
-                is_banned=False,
-                ban_reason="",
-            ),
-        )
-
-
     def new_group(self, id, title):
         return dict(
             id = id,
@@ -33,18 +22,53 @@ class Database:
                 reason="",
             ),
         )
-    
-    async def add_user(self, id, name):
-        user = self.new_user(id, name)
+   
+
+    def new_user(self, id):
+        return dict(
+            id = id,
+            ban_status=dict(
+                is_banned=False,
+                ban_reason="",
+            ),
+            join_date = datetime.date.today().isoformat()
+        )
+    async def add_user(self, id):
+        user = self.new_user(id)
         await self.col.insert_one(user)
-    
+
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id':int(id)})
         return bool(user)
-    
+
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
+
+    async def get_all_users(self):
+        return self.col.find({})
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_reason=''
+        )
+        user = await self.col.find_one({'id':int(id)})
+        if not user:
+            return default
+        return user.get('ban_status', default)
+
+
+    async def get_all_chats(self):
+        return self.grp.find({})
+
+    async def get_all_users(self):
+        return self.col.find({})
+
     
     async def remove_ban(self, id):
         ban_status = dict(
@@ -59,22 +83,6 @@ class Database:
             ban_reason=ban_reason
         )
         await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
-
-    async def get_ban_status(self, id):
-        default = dict(
-            is_banned=False,
-            ban_reason=''
-        )
-        user = await self.col.find_one({'id':int(id)})
-        if not user:
-            return default
-        return user.get('ban_status', default)
-
-    async def get_all_users(self):
-        return self.col.find({})
-    
-    async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
 
     async def get_banned(self):
         users = self.col.find({'ban_status.is_banned': True})
@@ -112,8 +120,6 @@ class Database:
         return count
     
 
-    async def get_all_chats(self):
-        return self.grp.find({})
 
 
     async def get_db_size(self):
