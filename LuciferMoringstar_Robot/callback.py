d4e0c4a1482e132b21ed402cbf4bebb3432b35b1
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-06-07 14:55:07 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -222,7 +222,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start=subscribe")
       
 
-        elif query.data.startswith("checkreturn
+        elif query.data.startswith("checkreturn"):
         
             if AUTH_CHANNEL and not await is_subscribed(client, query):
                 await query.answer("I Like Your Smartness, But Don't Be Oversmart 😒",show_alert=True)
