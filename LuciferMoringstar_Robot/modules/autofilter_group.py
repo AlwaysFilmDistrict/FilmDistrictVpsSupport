805ad796b07cd21b41ec8a4a127f7fca2a7cf42c
Anjan Modak <88320451+AnjanModak@users.noreply.github.com>
2022-06-16 20:41:34 +0530

    Update autofilter_group.py

--- LuciferMoringstar_Robot/modules/autofilter_group.py
+++ LuciferMoringstar_Robot/modules/autofilter_group.py
@@ -72,46 +72,6 @@ async def group_filters(client, message):
             await asyncio.sleep(60) # in seconds
             await LuciferMoringstar.delete()
             return
-        for i in "kishmish".split() :
-         if i in search.lower() :
-            LuciferMoringstar=await client.send_message(
-                text=f"""<b>Hello 👋 {message.from_user.mention},\n\n╭────────╮\n├• Request : Ignored ❌\n├• Reason : Available In Hall\n├• OTT : Not Available\n├• Date : Not Out\n├• Time: After OTT Released\n╰─────────────╯</b>""",
-                chat_id=message.chat.id,
-                reply_to_message_id=message.message_id,
-                parse_mode="html")
-            await asyncio.sleep(60) # in seconds
-            await LuciferMoringstar.delete()
-            return
-        for i in "belashuru".split() :
-         if i in search.lower() :
-            LuciferMoringstar=await client.send_message(
-                text=f"""<b>Hello 👋 {message.from_user.mention},\n\n╭────────╮\n├• Request : Ignored ❌\n├• Reason : Available In Hall\n├• OTT : Not Available\n├• Date : Not Out\n├• Time: After OTT Released\n╰─────────────╯</b>""",
-                chat_id=message.chat.id,
-                reply_to_message_id=message.message_id,
-                parse_mode="html")
-            await asyncio.sleep(60) # in seconds
-            await LuciferMoringstar.delete()
-            return
-         for i in "habji gabji".split() :
-          if i in search.lower() :
-            LuciferMoringstar=await client.send_message(
-                text=f"""<b>Hello 👋 {message.from_user.mention},\n\n╭────────╮\n├• Request : Ignored ❌\n├• Reason : Available In Hall\n├• OTT : Not Available\n├• Date : Not Out\n├• Time: After OTT Released\n╰─────────────╯</b>""",
-                chat_id=message.chat.id,
-                reply_to_message_id=message.message_id,
-                parse_mode="html")
-            await asyncio.sleep(60) # in seconds
-            await LuciferMoringstar.delete()
-            return
-        for i in "avijatrik".split() :
-         if i in search.lower() :
-            LuciferMoringstar=await client.send_message(
-                text=f"""<b>Hello 👋 {message.from_user.mention},\n\n╭────────╮\n├• Request : Ignored ❌\n├• Reason : Available In Hall\n├• OTT : Not Available\n├• Date : Not Out\n├• Time: After OTT Released\n╰─────────────╯</b>""",
-                chat_id=message.chat.id,
-                reply_to_message_id=message.message_id,
-                parse_mode="html")
-            await asyncio.sleep(60) # in seconds
-            await LuciferMoringstar.delete()
-            return
 
 
 
