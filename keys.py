6824002610f07db5675551bba1982309d8ea622d
Anjan Modak <88320451+AnjanModak@users.noreply.github.com>
2022-09-22 00:03:52 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,7 +2,7 @@
 API_ID = "7976184"
 API_HASH = "fd115c34d9a04c6bffd2a00f982afcd7"
 BOT_TOKEN = "5014982831:AAG2CsdZDIB8ew5vI9LFBGh9m6wde8DcpL0"
-BOT_PHOTO = "https://telegra.ph/file/23f37e8b4f6704a664acf.jpg"
+BOT_PHOTO = "https://graph.org/file/23f37e8b4f6704a664acf.jpg"
 BOT_USERNAME = "FilmDistrict_Bot"
 
 # Database
