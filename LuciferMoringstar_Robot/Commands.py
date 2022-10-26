96d56e942990719e9eede4f749801d0776e359ab
Anjan Modak <88320451+AnjanModak@users.noreply.github.com>
2022-10-27 01:09:48 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -224,6 +224,7 @@ DONATE_BUTTON = [[
 
 @Client.on_message(filters.command("donate"))
 async def donate(client, message):
+    await message.reply(text="DONATE_MESSAGE.format(mention=message.from_user.mention, message.from_user.id),
         reply_markup=InlineKeyboardMarkup(DONATE_BUTTON))
 
     if not await db.is_user_exist(message.from_user.id):
