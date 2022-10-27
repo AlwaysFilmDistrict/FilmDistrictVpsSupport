cc6771feb2133555c379ef03157163c70b5981d7
Mo-Tech <72255597+MoTechYT@users.noreply.github.com>
2022-10-27 08:02:29 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -230,6 +230,7 @@ async def donate_(client, message, callback):
         await message.reply(text=DONATE_MESSAGE.format(mention=message.from_user.mention if message.from_user else None),
             reply_markup=InlineKeyboardMarkup(DONATE_BUTTON))
     else:
+        await asyncio.sleep(5)
         await client.send_message(chat_id=message.from_user.id, text="Please /donate To Keep This Service Alive.") # format(mention=message.from_user.mention if message.from_user else None))
          #   reply_markup=InlineKeyboardMarkup(DONATE_BUTTON))
 
