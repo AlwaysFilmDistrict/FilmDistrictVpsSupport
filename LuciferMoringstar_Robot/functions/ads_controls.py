4bc5f4d906bd322ae39ac522d3a61ce14ac951d1
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-15 21:56:24 +0530

    Update ads_controls.py

--- LuciferMoringstar_Robot/functions/ads_controls.py
+++ LuciferMoringstar_Robot/functions/ads_controls.py
@@ -4,27 +4,21 @@ from configs import ADS_WEB_API, ADS_WEB_URL
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
+
 async def get_shortlink(link):
     https = link.split(":")[0]
     if "https" == https:
         https = "https"
         link = link.replace("http", https)
-    url = ADS_WEB_URL
+    url = f'{ADS_WEB_URL}'
     params = {'api': ADS_WEB_API,
               'url': link,
               }
 
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json()
-                if data["status"] == "success":
-                    return data['shortenedUrl']
-                else:
-                    logger.error(f"Error: {data['message']}")
-                    return f'https://{ADS_WEB_URL}/api?api={ADS_WEB_API}&link={link}'
-
-    except Exception as e:
-        logger.error(e)
-        return f'{ADS_WEB_URL}/api?api={ADS_WEB_API}&link={link}'
-
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+            data = await response.json()
+            if data["status"] == "success":
+                return data['shortenedUrl']
+            else:
+                return f"Error: {data['message']}"
