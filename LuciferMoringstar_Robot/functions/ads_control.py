8f844923a482680e5c31affecfcf6ddf8cb7cdc4
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-15 20:30:12 +0530

    Create ads_control.py

---
+++ LuciferMoringstar_Robot/functions/ads_control.py
@@ -0,0 +1,27 @@
+import aiohttp
+from configs import ADS_WEB_API, ADS_WEB_URI
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = ADS_WEB_URL
+    params = {'api': ADS_WEB_API,
+              'url': link,
+              }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://{ADS_WEB_URL}/api?api={ADS_WEB_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'{ADS_WEB_URL}/api?api={ADS_WEB_API}&link={link}'
+
