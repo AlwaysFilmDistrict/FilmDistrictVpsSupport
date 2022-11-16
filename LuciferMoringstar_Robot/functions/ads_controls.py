cb4ffb2e9aab5954210ae966be3a756cb3095d89
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-17 00:15:51 +0530

    Update ads_controls.py

--- LuciferMoringstar_Robot/functions/ads_controls.py
+++ LuciferMoringstar_Robot/functions/ads_controls.py
@@ -5,15 +5,23 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 async def get_shortlink(link):
-    url = f"{ADS_WEB_URL}/api"
+    url = f"{ADS_WEB_URL}"
     params = {
      'api': ADS_WEB_API,
      'url': link,
+     'format': 'json'
     }
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-            data = await response.json()
-            if data["status"] == "success":
-                return data['shortenedUrl']
-            else:
-                return f"Error: {data['message']}"
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json(content_type='text/html')
+                if data["status"] == "success":
+                    return data['shortlink']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://api.shareus.in/directLink?token={ADS_WEB_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'https://api.shareus.in/directLink?token={ADS_WEB_API}&link={link}'
