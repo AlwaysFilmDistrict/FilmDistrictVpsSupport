05aed808c2e0b40c5cd12583d2bf699c18d857eb
Anjan Modak <88320451+AnjanModak@users.noreply.github.com>
2022-11-16 22:33:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 from os import environ
 
 # Ads Control 
-ADS_WEB_API = environ.get('WEB_API', "SDBIJBelHsOnLrWPbYpVubRghB82")
-ADS_WEB_URL = environ.get('WEB_URL', "http://shareus.io")
+ADS_WEB_API = environ.get('WEB_API', "8723d8e36ffa5b85389c8ef8621186f53584d5f2")
+ADS_WEB_URL = environ.get('WEB_URL', "https://clickyfly.com")
