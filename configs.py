a42169dbe50e3d36fb18e76aaf812e326acf0289
Anjan Modak <88320451+AnjanModak@users.noreply.github.com>
2022-11-16 23:41:15 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 from os import environ
 
 # Ads Control 
-ADS_WEB_API = environ.get('WEB_API', "316c1eed1edd8b9ecdc5e4bc874b872bc1636f2e")
-ADS_WEB_URL = environ.get('WEB_URL', "https://gplinks.in")
+ADS_WEB_API = environ.get('WEB_API', "34fc6a4b0dda063ce59d03ea46822ea2b929a0b3")
+ADS_WEB_URL = environ.get('WEB_URL', "https://nanolinks.in")
