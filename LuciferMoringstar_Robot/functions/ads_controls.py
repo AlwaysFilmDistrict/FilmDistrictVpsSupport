6abc811698c21237cd08471dbf0fdc2d32e534d8
Mo-Tech <72255597+MoTechYT@users.noreply.github.com>
2022-11-16 08:27:35 +0530

    Update ads_controls.py

--- LuciferMoringstar_Robot/functions/ads_controls.py
+++ LuciferMoringstar_Robot/functions/ads_controls.py
@@ -4,17 +4,12 @@ from configs import ADS_WEB_API, ADS_WEB_URL
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-
 async def get_shortlink(link):
-    https = link.split(":")[0]
-    if "https" == https:
-        https = "https"
-        link = link.replace("http", https)
-    url = f'{ADS_WEB_URL}'
-    params = {'api': ADS_WEB_API,
-              'url': link,
-              }
-
+    url = f"{ADS_WEB_URL}/api"
+    params = {
+     'api': ADS_WEB_API,
+     'url': link,
+    }
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
             data = await response.json()
