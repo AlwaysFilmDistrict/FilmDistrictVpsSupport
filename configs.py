97fe3904262b4a6b5b7f8c7e8365e77dcf669660
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-17 00:13:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 from os import environ
 
 # Ads Control 
-ADS_WEB_API = environ.get('WEB_API', "34fc6a4b0dda063ce59d03ea46822ea2b929a0b3")
-ADS_WEB_URL = environ.get('WEB_URL', "https://nanolinks.in")
+ADS_WEB_API = environ.get('WEB_API', "SDBIJBelHsOnLrWPbYpVubRghB82")
+ADS_WEB_URL = environ.get('WEB_URL', "https://api.shareus.in/shortLink")
