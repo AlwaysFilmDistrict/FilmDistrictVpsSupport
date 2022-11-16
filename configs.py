06b73d629a61edbff4cea71338ee3288953d3f37
Anjan Modak <88320451+AnjanModak@users.noreply.github.com>
2022-11-16 22:46:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 from os import environ
 
 # Ads Control 
-ADS_WEB_API = environ.get('WEB_API', "8723d8e36ffa5b85389c8ef8621186f53584d5f2")
-ADS_WEB_URL = environ.get('WEB_URL', "https://clickyfly.com")
+ADS_WEB_API = environ.get('WEB_API', "4f10166e6699fae32780293cfa7281d7403f87ba")
+ADS_WEB_URL = environ.get('WEB_URL', "https://urlshortx.com")
