84456c4366f376adf72fa0f3c4a19b4da759d64f
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-17 22:35:46 +0530

    Create greetings.py

---
+++ LuciferMoringstar_Robot/functions/greetings.py
@@ -0,0 +1,16 @@
+import pytz, datetime
+from configs import TIME_ZONE
+
+async def user_greetings()
+    m = datetime.datetime.now(pytz.timezone(TIME_ZONE))
+    
+    if m.hour < 12:
+       Get = "Good Morning"
+    elif m.hour < 16:
+        Get = "Good Afternoon"
+    elif m.hour < 20:
+        Get = "Good Evening"
+    else:
+        Get = "Good Night"
+
+    return Get
