36f6fd6d3c71e02c7e8171c66deb0402c737d551
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-17 22:25:08 +0530

    Create scripts.py

---
+++ scripts.py
@@ -0,0 +1,11 @@
+SPELLING_MODE_TEXT = """
+<b>Hello 👋 {mention},
+
+I Couldn't 🔍 Find <code>{query}</code> You Asked For 🤷
+
+Click [GOOGLE] [IMDB] On Any Button And Find The Correct Movie/Series Name And Enter It Here 💯
+
+If You Do Not Receive The Movie/Series Even After Entering The Correct Name Then Your Requested Movie/Series Does Not Exit In My Database 🗄
+
+Or Requested Movie/Series Is Not Available For The 🆓 User. If Need It, Take 💸 Premium Membership. To Take It, Clicking The Button Below ⤵️</b>
+"""
