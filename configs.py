ca1c43547d765cbc7779f46490712a5ff5ce570d
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-17 22:44:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 from os import environ
-from scripts import SPELLING_MODE_TEXT
+from scripts import SPELLING_MODE_TEXT, CUSTOM_FILE_CAPTION
 
 
 
@@ -9,6 +9,7 @@ ADS_WEB_URL = environ.get('WEB_URL', "https://api.shareus.in/shortLink")
 
 # Customize 
 SPELLING_MODE_TEXT = environ.get('SPELLING_MODE_TEXT', SPELLING_MODE_TEXT)
+CUSTOM_FILE_CAPTION = environ.get('SPELLING_MODE_TEXT', CUSTOM_FILE_CAPTION)
 
 # other
 TIME_ZONE = "Asia/Kolkata"
