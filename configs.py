967cbb5c598b0a84e2c3746d6fb18284efb64f1a
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-17 22:48:50 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,8 @@
+from re import compile
 from os import environ
 from scripts import SPELLING_MODE_TEXT, CUSTOM_FILE_CAPTION
 
+search = compile(r'^.\d+$')
 
 
 # Ads Control 
@@ -10,6 +12,10 @@ ADS_WEB_URL = environ.get('WEB_URL', "https://api.shareus.in/shortLink")
 # Customize 
 SPELLING_MODE_TEXT = environ.get('SPELLING_MODE_TEXT', SPELLING_MODE_TEXT)
 CUSTOM_FILE_CAPTION = environ.get('SPELLING_MODE_TEXT', CUSTOM_FILE_CAPTION)
+
+# users & admins
+FORWARD_PERMISSION = [int(admin) if search.search(admin) else admin for admin in environ.get('FORWARD_PERMISSION', "919653750").split()]
+
 
 # other
 TIME_ZONE = "Asia/Kolkata"
