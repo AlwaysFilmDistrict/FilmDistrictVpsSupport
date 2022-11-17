4a5b801f702bc8fde71fc311ab5f8cd0ae6ac3da
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-17 22:43:26 +0530

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -9,3 +9,16 @@ If You Do Not Receive The Movie/Series Even After Entering The Correct Name Then
 
 Or Requested Movie/Series Is Not Available For The 🆓 User. If Need It, Take 💸 Premium Membership. To Take It, Clicking The Button Below ⤵️</b>
 """
+
+CUSTOM_FILE_CAPTION = """
+╭──[ミ★ FILM DISTRICT ★彡]──╮
+├• 👋 𝐇𝐞𝐥𝐥𝐨 {mention}
+├• ✅ 𝐘𝐨𝐮𝐫 𝐅𝐢𝐥𝐞 𝐈𝐬 𝐑𝐞𝐚𝐝𝐲
+├• 🎬 𝐓𝐢𝐭𝐥𝐞 : <code>{file_name}</code>
+├• 💾 𝐒𝐢𝐳𝐞 : <code>{file_size}</code>
+├• 🔘 𝐉𝐨𝐢𝐧 🎗 𝐒𝐡𝐚𝐫𝐞 🎗 𝐒𝐮𝐩𝐩𝐨𝐫𝐭
+├• 🔗 <a href="https://telegram.me/joinchat/BOMKAM_4u0ozNWU1">𝐅𝐢𝐥𝐦 𝐃𝐢𝐬𝐭𝐫𝐢𝐜𝐭</a>
+├• 📣 <a href="https://telegram.me/joinchat/EUUS8b0iEnVjZTU9">𝐅𝐢𝐥𝐦 𝐃𝐢𝐬𝐭𝐫𝐢𝐜𝐭 𝐔𝐩𝐝𝐚𝐭𝐞𝐬</a>
+├• ⬆️ 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 <a href="https://telegram.me/helloheartbeat">𝐇𝐞𝐚𝐫𝐭𝐁𝐞𝐚𝐭</a>
+╰──────[ 👑 ]───────╯
+"""
